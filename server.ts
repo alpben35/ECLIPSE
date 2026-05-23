609286735845299b52398c6d50d3bce44dc49f03
alpben35 <76075818+alpben35@users.noreply.github.com>
2026-05-23 22:23:24 +0100

    -

--- server.ts
+++ server.ts
@@ -210,7 +210,7 @@ function getDb(): admin.firestore.Firestore {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.set('trust proxy', 1);
 
